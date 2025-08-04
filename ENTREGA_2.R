450b52077019573cbc7758abf77d55203acd22ff
VeroGonzA <gonzalez.acevedo.v@gmail.com>
2025-08-03 20:19:07 -0400

    ndskajs

--- ENTREGA_2.R
+++ ENTREGA_2.R
@@ -1,1 +1,6 @@
-#FLHADNASNDLAKSD
+library(readr)
+bbdd<- read_delim("data/base-de-datos-ele7 (1).csv", 
+                                    delim = ";", escape_double = FALSE, locale = locale(encoding = "latin1"), 
+                                    trim_ws = TRUE)
+
+
