5c204d6d9be1e1b8ad98b74403d092d1ec378a86
VeroGonzA <gonzalez.acevedo.v@gmail.com>
2025-08-07 19:32:59 -0400

    anoto preguntas

--- Taller II/Taller2.R
+++ Taller II/Taller2.R
@@ -1,1 +1,31 @@
-####PREGUNTA I
+####PARTE I####
+####PREGUNTA 1####
+#Recategorizar las siguientes variables:
+#Años de educación en dos grupos: Ed.media <=12 versus >12 Ed.superior
+#Edad en tres grupos: joven <30; adulto 30-50; mayor >50.
+
+#(a)#Obtenga una descripción de las variables cuantitativas: experiencia laboral e ingreso según sexo, sector laboral y edad categorizada.
+#(b)#Obtenga tabla % que permita inferir potencial asociación entre el nivel educacional y sector laboral.
+
+#### PREGUNTA 2####
+#Test de hipótesis (e idealmente represente en forma gráfica) (1 punto)
+#(a)#Es válida la afirmación: ¿existe discriminación salarial según sexo?
+# (b)#Es válida la afirmación: ¿A mayor educación mayor ingreso?
+#  (c)
+#¿Existe diferencia en el salario según edad (categorizada)?
+#  En cada caso indique sus resultados – test, valor-p y medias y sus errores estándar.
+
+
+#Ajuste modelos lineales para ingreso (2 puntos)
+#(a)
+#Lleve a cabo los modelos de regresión lineal simple del ingreso en función de experiencia laboral, edad, educación, sexo. Con base a los resultados obtenidos, ¿qué variables son estadísticamente significativas?
+#  (b)
+#Obtenga un modelo de regresión lineal múltiple en función de todas las variables previas que resultaron estadísticamente significativas. ¿Siguen siendo significativas?
+#  (c)
+#Son válidos los supuestos en este último modelo.
+#4.
+#Repita todo [3] pero usando como variable respuesta log(ingreso) (2 puntos)
+#(a)
+#¿hay diferencias respecto a lo obtenido en 3a, 3b y 3c? ¿cuáles?
+#  Trate de ser lo más conciso al responder cada pregunta. En su análisis debe considerar lo visto en el curso y en especial lo relativo a análisis de residuos.
+
