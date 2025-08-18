ee2cf5e12f3a7f65bf217065d7f346fb8e93fa1a
VeroGonzA <gonzalez.acevedo.v@gmail.com>
2025-08-18 01:52:22 -0400

    copiado de los apuntes de la entrega 2

---
+++ Analisis pendientes.R
@@ -0,0 +1,23 @@
+#Ideas de análisis y cruces: 
+#  
+#  Caracterizar a las empresas desde la participación femenina. 
+#
+#Datos de la empresa en la base 
+#
+#Caracterizar a la empresa por tamaño y rubro + antigüedad, empresa nacional extranjera, estatal.  
+#
+#Participación de los directorios:  tabla check. Falta interpretacion u otra desagregacion 
+#
+#Distribución de la dotación por tipo de ocupación: check, está remuneraciones segun ocupacion y sexo + número de personas segun ocupacion y sexo. 
+#
+#INCLUIR DATOS DE LOS QUE NO CUMPLEN CON ALGUNA CARACTERISTICA. 
+#
+#Proporción de mujeres en cargos directivos X tipo de empresa. 
+#
+#Media y desviación estándar de la dotación femenina según tamaño de empresa. 
+#
+#Moda del nivel educacional de mujeres en cargos estratégicos. 
+#
+#Frecuencia de empresas con directorio y gerente general mujer. 
+#
+#Distribución por tamaño de la empresa, distribución por salario, distribución por ocupación.  
